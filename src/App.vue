8f8cddf03751217bb1fe855aea44ba21b9722ca0
Balakhonskaya <lisa.balakhonskaya@diva-e.com>
2024-11-21 10:35:18 +0100

    Replace defalut content

--- src/App.vue
+++ src/App.vue
@@ -66,4 +66,40 @@ ul {
   height: 30px;
   margin: 0 10px 10px 0;
 }
+
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1400px;
+  min-height: 300px;
+}
+
+header {
+  background-color: #999;
+  width: 1460px;
+  margin: 0 auto;
+}
+
+ul {
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+
+.logo {
+  vertical-align: middle;
+  height: 30px;
+  margin: 0 10px 10px 0;
+}
 </style>
