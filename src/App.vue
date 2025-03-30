e57d9c4d95927026e2f87aa6edd4d7077507c063
ancc02 <oandresccc@gmail.com>
2025-03-29 21:33:22 -0500

    Create RobotBuilder

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
+<header>
+  <nav>
+    <ul>
+      <li class="nav-item">
+        <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo">
+        Build a bot
+      </li>
+    </ul>
+  </nav>
+</header>
   <main>
-   <HomePage msg="Welcome to Your Vue.js App"/>
+   <!-- <HomePage msg="Welcome to Your Vue.js App"/> -->
+   <RobotBuilder />
   </main>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'App',
   components: {
-    HomePage,
+    RobotBuilder,
   },
 };
 </script>
@@ -21,6 +33,38 @@ export default {
 }
 
 body{
-  background:linear-gradient(to bottom,#555,#999)
+  background:linear-gradient(to bottom,#555,#999);
+  background-attachment: fixed;
+}
+
+main{
+  margin: 0 auto;
+  padding: 30 px;
+  background-color: white;
+  width: 1400px;
+  min-height: 300px;
+}
+
+header{
+  background-color: #999;
+  width: 1460px;
+  margin: 0 auto;
+}
+
+ul{
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item{
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+
+.logo{
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
---
+++ src/home/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="Home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" aria-hidden="true" alt="Robot" />
+    </div>
+    <div class="get-started">
+      <a href>get started</a> building your first robot
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+
+.robot {
+  height: 500px;
+}
+
+.get-started {
+  padding-top: 20px;
+  font-size: 32px;
+}
+</style>
