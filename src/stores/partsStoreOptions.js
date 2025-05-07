0b1ab10283990652ba4c0af3c6bd420f622402bc
ancc02 <oandresccc@gmail.com>
2025-05-07 18:49:37 -0500

    add options API store with pinia

---
+++ src/stores/partsStoreOptions.js
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia';
+
+export default defineStore('parts', {
+    state: () => ({
+        parts: null,
+    }),
+
+    actions:{
+        async getParts(){
+            const response = await fetch('/api/parts');
+            this.parts = await response.json();
+        },
+    },
+});
